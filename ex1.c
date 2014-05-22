834f1622bc8e6fbda0c5b32d2669901eb3f79e79
felixlungu <felix.lungu@gmail.com>
2014-05-22 14:41:59 +0300

    Create ex1.c

---
+++ ex1.c
@@ -0,0 +1,16 @@
+// \l ex1.c
+// q will intercept the k) in comment bellow and will trigger the compilation and binding for add and sub
+
+K add(K x,K y){R k(0,"{x+y}",r1(x),r1(y),(K)0);}
+K sub(K x,K y){R k(0,"{x-y}",r1(x),r1(y),(K)0);}
+
+
+/*see the last line for the trick to foul the q interpreter and still have valid c comment. this line is considered a comment by q and it is not passed to .c.e 
+k)add:.c.dl[`add;2]
+k)sub:.c.dl[`sub;2]
+k)-1"calling add[2;3]";
+k)add[2;3]
+k)-1"calling sub[2;3]";
+k)sub[2;3]
+/ */
+
